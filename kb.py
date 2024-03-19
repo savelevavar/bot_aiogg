ee413c70e0919a66aeacf1fe19786c2c61d9b23f
savelevavar <savelevavar@mail.ru>
2024-03-19 18:38:37 +0500

    Initial commit

---
+++ kb.py
@@ -0,0 +1,11 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+
+
+menu = [
+    [InlineKeyboardButton(text="Просмотр доступных изображений", callback_data="check_pictures")]
+]
+
+
+menu = InlineKeyboardMarkup(inline_keyboard=menu)
+exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Выйти в меню")]], resize_keyboard=True)
+iexit_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="◀️ Выйти в меню", callback_data="menu")]])
